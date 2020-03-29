fd6d196450ef8dd1ccf53bf053393177ac31dbe7
Quin <quin@cheesits456.dev>
2020-03-29 11:05:52 -0700

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron"),
+  path = require("path");
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile(path.join("render", "index.html"));
+
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
