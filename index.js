fe09c689779d907fd00f8e0bd6b9b101276f825b
Quin F. <quin@cheesits456.dev>
2025-08-25 10:16:10 -0700

    Remove console.log("test")

--- index.js
+++ index.js
@@ -30,7 +30,3 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
-
-console.log("test");
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
