ec8fa7441ad3d17c52ba8448907b6f11b689d7ae
Quin F. <quin@cheesits456.dev>
2025-08-25 09:57:10 -0700

    Move version number

--- page/js/index.js
+++ page/js/index.js
@@ -1,3 +1,11 @@
+const path = require("path");
+
+const buildData = require(path.join("..", "data.json"));
+const package = require(path.join("..", "package.json"));
+
+// Set version number
+document.getElementById("version").innerHTML = `${buildData.type}${package.version} build ${buildData.build}`;
+
 var payments = {
 	1: 'Credit Card',
 	2: 'Check',
