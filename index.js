9a0540302369fa5e7502bb6e8df29f4c58af1b5f
Quin F. <quin@cheesits456.dev>
2025-08-25 07:55:55 -0700

    change tab type

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const { app, BrowserWindow, ipcMain } = require("electron"),
-  path = require("path");
+	path = require("path");
 
 function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 1065,
-    height: 800,
-    minWidth: 1065,
-    minHeight: 800,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  })
+	// Create the browser window.
+	const win = new BrowserWindow({
+		width: 1065,
+		height: 800,
+		minWidth: 1065,
+		minHeight: 800,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false
+		}
+	})
 
-  // and load the index.html of the app.
-  // win.setMenu(null);
-  win.maximize();
-  win.loadFile(path.join("render", "index.html"));
+	// and load the index.html of the app.
+	// win.setMenu(null);
+	win.maximize();
+	win.loadFile(path.join("page", "index.html"));
 
-  win.webContents.on('new-window', function(e, url) {
-    e.preventDefault();
-    require('electron').shell.openExternal(url);
-  });
+	win.webContents.on('new-window', function (e, url) {
+		e.preventDefault();
+		require('electron').shell.openExternal(url);
+	});
 
 }
 
