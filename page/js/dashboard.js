98f763cfc71b30261dc0593be7b76b257fccccba
Quin F. <quin@cheesits456.dev>
2025-08-25 11:27:11 -0700

    Set all stats at the same time

--- page/js/dashboard.js
+++ page/js/dashboard.js
@@ -7,21 +7,18 @@ updateStats();
 setInterval(updateStats, 3000)
 
 async function updateStats() {
-	// Set stats
-	document.getElementById("stat-ram").innerHTML = `${Math.round(os.totalmem() / 1024 / 1024 / 1024 * 100) / 100} GB`;
-
+	// Get stats
 	let cpu = await systeminformation.cpu();
-	document.getElementById("stat-cpu").innerHTML = `${cpu.cores} cores @<br>${cpu.speedmax} GHz<br>`;
-
 	let drive = await osu.drive.info();
-	document.getElementById("stat-storage").innerHTML = `${drive.totalGb} GB`;
-
-	document.getElementById("stat-user").innerHTML = require("os").userInfo().username;
-
-
 	let memUsage = await osu.mem.info();
 	let cpuUsage = await osu.cpu.usage();
 	let diskUsage = await osu.drive.info();
+
+	// Set stats
+	document.getElementById("stat-ram").innerHTML = `${Math.round(os.totalmem() / 1024 / 1024 / 1024 * 100) / 100} GB`;
+	document.getElementById("stat-cpu").innerHTML = `${cpu.cores} cores @<br>${cpu.speedmax} GHz<br>`;
+	document.getElementById("stat-storage").innerHTML = `${drive.totalGb} GB`;
+	document.getElementById("stat-user").innerHTML = require("os").userInfo().username;
 
 	document.getElementById("usage-ram").innerHTML = `${100 - memUsage.freeMemPercentage}%`;
 	document.getElementById("usage-cpu").innerHTML = `${cpuUsage}%`;
@@ -30,7 +27,13 @@ async function updateStats() {
 }
 
 function timeConvert(seconds) {
-	hours = Math.floor(seconds / 60 / 60);
-	minutes = Math.floor((seconds - (hours * 60 * 60)) / 60);
-	return `${hours}h ${minutes}m`;
+	if (seconds > 60 * 60 * 24) {
+		let days = Math.floor(seconds / 60 / 60 / 24);
+		let hours = Math.floor((seconds - (days * 60 * 60 * 24)) / 60 / 60);
+		return `${days}d ${hours}h`;
+	} else {
+		let hours = Math.floor(seconds / 60 / 60);
+		let minutes = Math.floor((seconds - (hours * 60 * 60)) / 60);
+		return `${hours}h ${minutes}m`;
+	}
 }
