5423796c2894025faa93368506edc24d52982b84
Quin F. <quin@cheesits456.dev>
2025-08-28 19:40:04 -0700

    Update find-duplicates.js

--- page/js/find-duplicates.js
+++ page/js/find-duplicates.js
@@ -5,27 +5,28 @@ Array.prototype.remove = function (a) {
 
 
 // Require mandatory packages
-fs = require("fs");
-path = require("path");
-stats = 0;
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+let stats = 0;
 
 // Set panel height
-document.getElementById("panel-progress").style.height = `${document.getElementById("panel-folder").offsetHeight}px`
+document.getElementById("panel-progress").style.height = `${document.getElementById("panel-folder").height}px`
 
 
 // Set defualt search directory
-document.getElementById("search-dir").value = require("path").join(process.env.USERPROFILE, "Documents");
+document.getElementById("search-dir").value = os.homedir();
 
 
 // Define onClick functions
-function getDir() {
+function getDirDuplicates() {
 	const { dialog } = require('electron').remote;
 	dialog.showOpenDialog({
 		properties: ['openDirectory']
 	}).then(dir => document.getElementById("search-dir").value = dir.filePaths[0]);
 }
 
-function scan() {
+function scanDuplicates() {
 	document.getElementById("button-scan").setAttribute("onClick", "Metro.toast.create('There is already a scan in progress')");
 	stats = 0;
 	const dir = document.getElementById("search-dir").value,
@@ -35,7 +36,7 @@ function scan() {
 	document.getElementById("status").innerHTML = `<span class="text-medium">Scanning . . .</span>`;
 	document.getElementById("step1").innerHTML = "<br><span class='text-medium'>Step 1/3:</span> Building file list [0 files found]";
 
-	walk(dir, async function (err, results) {
+	walkDuplicates(dir, async function (err, results) {
 		if (err) throw err;
 
 		document.getElementById("step2").innerHTML = "<span class='text-medium'>Step 2/3:</span> Calculating file hashes [0%]";
@@ -61,7 +62,7 @@ function scan() {
 			if (files.length > 1) {
 				let push = [];
 				for (const file of files) {
-					push.push(`<div id="${file}" class="text-secondary"><span class="mif-bin mif-lg fg-red hover-pointer" onClick="deleteFile('${file.replace(/\\/g, "\\\\")}')"></span> ${file}</div>`);
+					push.push(`<div id="${file}" class="text-secondary"><span class="mif-bin mif-lg fg-red hover-pointer" onClick="deleteFileDuplicates('${file.replace(/\\/g, "\\\\")}')"></span> ${file}</div>`);
 				}
 
 				await setTimeout(() => {
@@ -76,7 +77,7 @@ function scan() {
 
 
 // Define helper functions
-function walk(dir, done) {
+function walkDuplicates(dir, done) {
 	let results = []
 	fs.readdir(dir, (err, list) => {
 		if (err) return done(err);
@@ -86,7 +87,7 @@ function walk(dir, done) {
 			file = path.resolve(dir, file);
 			fs.stat(file, (err, stat) => {
 				if (stat && stat.isDirectory()) {
-					walk(file, (err, res) => {
+					walkDuplicates(file, (err, res) => {
 						results = results.concat(res);
 						if (!--pending) done(null, results);
 					});
@@ -119,7 +120,7 @@ function fileHash(filename, algorithm = "sha256") {
 	});
 }
 
-function deleteFile(file) {
+function deleteFileDuplicates(file) {
 	fs.unlink(file, err => {
 		if (err) return Metro.toast.create("Missing permission to delete this file");
 		document.getElementById(file).style.display = "none";
