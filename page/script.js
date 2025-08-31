d42aec769d65345ad82347705a55a02d9d42892a
Quin F. <quin@cheesits456.dev>
2025-08-30 20:08:33 -0700

    Create new base GUI design

---
+++ page/script.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+const buildData = require(path.join("..", "data.json"));
+const package = require(path.join("..", "package.json"));
+
+// Set version number
+document.getElementById("version").innerHTML = `${buildData.type}${package.version} build ${buildData.build}`;
