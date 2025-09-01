f2a98ea4487a127bcaef0be679f68c8756bcb6d4
Quin F. <quin@cheesits456.dev>
2025-09-01 12:12:43 -0700

    Remove unnecessary comment

--- index.js
+++ index.js
@@ -25,7 +25,4 @@ function createWindow() {
 
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 electron.app.whenReady().then(createWindow);
